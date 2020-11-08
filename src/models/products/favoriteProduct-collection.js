467bfc1293e66632225d1347af80960f7e2c2750
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-11-08 18:35:44 +0200

    finish favorite

--- src/models/products/favoriteProduct-collection.js
+++ src/models/products/favoriteProduct-collection.js
@@ -4,27 +4,36 @@ require('dotenv').config();
 const client = require('../pool');
 
 class Favorite {
-  async addToFavorite(productID, product) {
-    const selectQuery = 'SELECT p_id from buyer_favorite where id=$1';
-    let safeValues = [productID.id];
-    let productDb = await client.query(selectQuery, safeValues);
-    console.log('productDb', productDb.rows[0]);
-    if (!productDb.rows[0]) {
+  async addToFavorite(productID, userId) {
+    const selectQuery = 'SELECT * from buyer_favorite where id=$1';
+    let safeValues = [productID];
+    let productDb = await client
+      .query(selectQuery, safeValues)
+      .then((result) => result.rows[0]);
+    if (!productDb) {
       const insertQuery =
         'INSERT INTO buyer_favorite (u_id,is_deleted,p_id) VALUES ($1,$2,$3) RETURNING *';
-      let safeValues = [product.u_id, false, product.p_id];
-      let productInfo = await client.query(insertQuery, safeValues);
-      return productInfo.rows[0];
+      let safeValues = [userId, false, productID];
+      let productInfo = await client
+        .query(insertQuery, safeValues)
+        .then((result) => result.rows[0]);
+      return productInfo;
     } else {
-      return 'this product is not bought yet';
+      if (productDb.is_deleted) {
+        return await this.delete(productDb.id);
+      } else {
+        return 'You have this product in your Favorite';
+      }
     }
   }
 
-  async delete(deleteId, favoriteStatus = true) {
-    let deleteQuery = `update buyer_favorite set is_deleted=$2 where id=$1 RETURNING *;`;
-    let safeValues = [deleteId, favoriteStatus];
-    let productDeleting = await client.query(deleteQuery, safeValues);
-    return productDeleting.rows[0];
+  async delete(deleteId) {
+    let deleteQuery = `update buyer_favorite set is_deleted= not is_deleted where id=$1 RETURNING *;`;
+    let safeValues = [deleteId];
+    let productDeleting = await client
+      .query(deleteQuery, safeValues)
+      .then((result) => result.rows[0]);
+    return productDeleting;
   }
 }
 module.exports = new Favorite();
