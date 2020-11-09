3ac523496e15500e36cc7dd8720c376855cabf52
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-11-09 09:41:39 +0200

    write some tests

--- data/drop_n_create.js
+++ data/drop_n_create.js
@@ -5,14 +5,14 @@ require('dotenv').config();
 const DROP = process.env.DROP;
 
 // Change the name to your data base
-pgtools.dropdb(DROP, 'test1150', function (err, res) {
+pgtools.dropdb(DROP, 'testsport', function (err, res) {
   if (err) {
     console.error(err);
     process.exit(-1);
   }
   console.log(res);
 
-  pgtools.createdb(DROP, 'test1150', function (err, res) {
+  pgtools.createdb(DROP, 'testsport', function (err, res) {
     if (err) {
       console.error(err);
       process.exit(-1);
